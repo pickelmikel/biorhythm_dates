3c60008b1e82626c3764e3d9d33dd8fbe38afd20
Michael Kramer <pickelmikel@gmail.com>
2026-03-18 21:49:18 -0700

    Started compatibility checker page

--- biorhythm_checker.py
+++ biorhythm_checker.py
@@ -1,13 +1,35 @@
+from datetime import date
+import numpy as np
 import streamlit as st
 
-st.title('Biorhythm Checker')
-st.info('Coming soon...until then push the buttons')
+@st.cache_data
+def bio_compat(your_birth, other_birth):
+    T = {'Emotional':28, 'Intellectual':33, 'Physical':23}
+    t = your_birth - other_birth
+    cycle_data = {}
+    for cycle, value in T.items():
+        form = np.cos(np.pi * t.days / value)
+        cycle_data[cycle] = abs(form)
+        form_final = form * 100
+        st.write(f"{cycle} Cycle Percent Compatibility: {abs(form_final):.2f}")
+    #return cycle_data, np.array([*cycle_data.values()]).mean() * 100
+    cycle_mean = np.array([*cycle_data.values()]).mean() * 100
+    st.write(f'Overall Compatibility: {cycle_mean} Percent')
+  
+## -- MAIN DISPLAY CODE -- ##
+st.title('Biorhythm Compatibility Checker')
 
-def have_ball():
-    st.balloons()
+birth_date = st.date_input('Select your birthdate',
+                           #value=date(2000,1,1),
+                           min_value=date(1900,1,1),
+                           max_value=date.today(),
+                           key='your_birhdate',
+                           format='YYYY-MM-DD')
+other_date = st.date_input('Select other birthdate',
+                           #value=date(2000,1,1),
+                           min_value=date(1900,1,1),
+                           max_value=date.today(),
+                           key='other_birthdate',
+                           format='YYYY-MM-DD')
 
-def have_snow():
-    st.snow()
-    
-st.button('Click me!', on_click=have_ball, key='ball')
-st.button('Click me!', on_click=have_snow, key='snow')
+bio_compat(birth_date,other_date)
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 perfect_compat_finder = st.Page('perfect_compatibility_finder.py',
     title='Perfect Compatibility Finder')
 biorhythm_checker = st.Page('biorhythm_checker.py',
-    title='Biorhythm Checker')
+    title='Biorhythm Compatibility Checker')
 pg = st.navigation(
     pages=[perfect_compat_finder, biorhythm_checker],
     position='top',
